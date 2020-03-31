8a4f4c21c7c83ed28c68318f5988fe4b7125194d
BrandoJesus <brandojesus011@gmail.com>
2020-03-31 12:16:26 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Login Angular';
+  name = 'Login Angular APP';
 
   
 }
